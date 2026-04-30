2068a063713cd122d98b73b8c973cbc2e8763158
PinakiNandanRout <pinakinandanrout7381@gmail.com>
2026-04-30 07:42:02 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "null"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33"=> 33
+// "33abc" =>nan // not an number
+//true => 1; false =>0
+
+let isLoggedIn ="pi"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=>true; 0=> false
+//"" => false
+// "hitesh" => true
+
+let somenumber =33
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console. log(typeof stringNumber)

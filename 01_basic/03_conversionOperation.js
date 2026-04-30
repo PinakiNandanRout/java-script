87d73dcf86e319d83ae50985db44bf1fa8f82319
PinakiNandanRout <pinakinandanrout7381@gmail.com>
2026-04-30 11:08:18 +0000

    conversion Operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -22,5 +22,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let somenumber =33
 
 let stringNumber = String(somenumber)
-console.log(stringNumber);
-console. log(typeof stringNumber)
+//console.log(stringNumber);
+//console. log(typeof stringNumber)
+
+//********************** Operation *******************************
+
+let value =3
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2);
+//conosle.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+/*let str1 = "hello"
+let str2 = " pinaki"
+
+let str3 = str1 + str2
+console.log(str3)
+ 
+console.log("1" + 2);
+console.log("1"+ "2");
+console.log("1"+2 + 2);
+console.log(1+2 + "2"); */
+
+console.log(+true);
+//console.log(true+);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter; // prefix postfix
+console.log(gameCounter);
+
+//link to study 
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+

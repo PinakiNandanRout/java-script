e7b480cff85646256464e429f6251ba4e7a680fc
PinakiNandanRout <pinakinandanrout7381@gmail.com>
2026-04-30 05:48:13 +0000

    variable uses

---
+++ 01_basic/variable.js
@@ -0,0 +1,16 @@
+const accountId = 144533
+let accountEmail = "hero@google.com"
+var accountpassword = "122334"
+accountCity = "balasore"
+
+// accountId = 2//not allowed
+accountEmail = "hfeghfg@g.com"
+accountpassword = "gydfggfygs"
+accountCity = "bengaluru"
+let accountstate; // semicolon optional
+
+
+console.log(accountId);
+
+
+console.table([accountId,accountEmail,accountpassword,accountCity,accountstate])

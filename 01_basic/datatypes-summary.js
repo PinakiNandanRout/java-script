b186973ac88a7ecdab0c393b4f6531e596950a3f
PinakiNandanRout <pinakinandanrout7381@gmail.com>
2026-05-02 09:00:31 +0000

    datatypes - summary

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,39 @@
+// two types - primitive 2. non primitive
+//call by value  / call by reference how data store how access
+
+// primitive 
+
+// 7 types : string,Number,Boolean,null,undefined,symbol,BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail ;
+const id = symbol('123')
+const anotherid = symbol ('123')
+
+
+console.log(id === anotherId);
+
+const bigNumber = 23242423453455251n // n for bigint big number
+
+
+
+//Reference (Non primitive)
+
+// Array, objects ,function
+
+const heros = ["shaktiman","naagraj","doga"];
+ let myObj = {
+     name: "pinaki"
+     age :22,
+}
+const myfunction = function(){
+    console.log("hello world");
+
+}
+
+console.log(typeof myFunction);
+
